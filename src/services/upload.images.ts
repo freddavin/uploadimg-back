bcd27685c82d381b5d98c41c946da00ba3405868
Frederico Ferreira <fredericodavin@gmail.com>
2024-05-07 18:37:39 -0300

    crefac: remove upload with mongo

--- src/services/upload.images.ts
+++ src/services/upload.images.ts
@@ -1,11 +1,11 @@
 import { randomUUID } from 'crypto';
-import { Image } from '../models';
+// import { Image } from '../models';
 import { uploadImageToDrive } from '../libs/google.drive';
 
 export const uploadImages = async (urls: string[]) => {
   const promises = urls.map(async (url) => {
     const id = randomUUID();
-    await Image.create({ id, url });
+    // await Image.create({ id, url });
 
     const [base64Type, base64Image] = url.split(';base64,');
     if (!base64Image) {
